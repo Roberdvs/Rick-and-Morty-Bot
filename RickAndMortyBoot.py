7396ce43e9f2eb3dd4ea17525edba1436888a0fd
Roberdvs <roberdvs@gmail.com>
2015-10-29 15:16:58 +0100

    Update RickAndMortyBoot.py

--- RickAndMortyBoot.py
+++ RickAndMortyBoot.py
@@ -17,33 +17,28 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 while(1):
-    twt = api.search(q="Rick and Morty")     
- 
+    twt = api.search(q="Rick and Morty", rpp= 10)
+
 #list of specific strings we want to check for in Tweets
-    t = ['rick and morty',
-        'Rick and Morty']
+
     for s in twt:
-        for i in t:
-            if  i in s.text:
-                sn = s.user.screen_name
-                rdm=random.randint(0, len(quotes) -1) #Número aleatorio de quote
-                m = "@%s " % (sn) + quotes[rdm]
-                if len(m)>140:                      #Si user + mensaje supera 140 caracteres le damos otra oportunidad de frase.    
-                    rdm=random.randint(0, len(quotes) -1) #Número aleatorio de quote
-                    m = "@%s " % (sn) + quotes[rdm]
-                print (m)
-                
-                filename=open('history.txt','r')
-                historial=filename.read()
-                filename.close()
-                if m not in historial:
-                    filename=open('history.txt','a')
-                    filename.write(m)
-                    filename.close
-                    try:
-                        s = api.update_status(m, s.id)
-                    except tweepy.TweepError as e:
-                        print (e)
-                    time.sleep(150)
-    time.sleep(150)
-time.sleep(1800)
+        sn = s.user.screen_name
+        rdm=random.randint(0, len(quotes) -1) #Numero aleatorio de quote
+        m = "@%s " % (sn) + quotes[rdm]
+        if len(m)>140:                      #Si user + mensaje supera 140 caracteres le damos otra oportunidad de frase.
+            rdm=random.randint(0, len(quotes) -1) #Numero aleatorio de quote
+            m = "@%s " % (sn) + quotes[rdm]
+        print (m)
+
+        filename=open('history.txt','r')
+        historial=filename.read()
+        filename.close()
+        if m not in historial:
+            filename=open('history.txt','a')
+            filename.write(m)
+            filename.close
+            try:
+                s = api.update_status(status=m, in_reply_to_status_id=s.id)
+            except tweepy.TweepError as e:
+                print (e)
+            time.sleep(200)
