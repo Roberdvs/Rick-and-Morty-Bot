257ad6e3baaf5be615e4b2f072acfad33a6ad9f0
Roberdvs <roberdvs@gmail.com>
2015-11-16 18:00:42 +0100

    Update keys.py

--- keys.py
+++ keys.py
@@ -2,8 +2,8 @@
 #keys.py
 #visit http://dev.twitter.com to create an application and get your keys
 keys = dict(
- consumer_key =          'c7VEXdwcVt9ObTjwbQL4wB623',
- consumer_secret =       'vhg2tdl4ryLYgyOwt2N8LTOU60bzV5lW0EXRp1hjMbWFJQNSBU',
- access_token =          '4062297627-MThxnWC4fhGxALZYRWviCU46qrmxCTsEc1I1tyd',
- access_token_secret =   'FGbcIzWpyZreClIfMM28vosCG8S6vEJIVtVrknx8ScVMQ',
+ consumer_key =          '********************',
+ consumer_secret =       '********************',
+ access_token =          '********************',
+ access_token_secret =   '********************',
 )
